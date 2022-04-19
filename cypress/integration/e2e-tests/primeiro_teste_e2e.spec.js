b279b260450f810bc55eca2cee486f289b4b5d35
Dieferson da Silva Cruz <dieferson.cruz@dextra-sw.com>
2022-04-19 09:59:05 -0300

    Removido page objects

--- cypress/integration/e2e-tests/primeiro_teste_e2e.spec.js
+++ cypress/integration/e2e-tests/primeiro_teste_e2e.spec.js
@@ -1,38 +1,30 @@
 /// <reference types="Cypress" />
 
-import cadastro_usuarios_page from '../../support/pageObjects/cadastro_usuario_page';
+const editUsuario = 'input[name="form_usuario"';
+const editSenha = 'input[name="form_senha"';
+const editNome = 'input[name="form_nome"';
+const btnEnviar = '.btn-info';
+const tableUsuarios = 'table[border="1"]';
 
-const cadastroUsuarioPage = new cadastro_usuarios_page;
-var listaUsuarios = [];
 
-describe("Cadastro de usuários", function(){
-    before(() => {
-        cy.fixture("dadosUsuarios.json").then(dadosUsuarios =>{
-            listaUsuarios = dadosUsuarios;
+describe("Cadastro de usuários", function () {
+    context("Quando quando informo dados válidos", function () {
+        it("Deve retornar um cadastro válido", function () {
+            cy.visit("http://aprendendotestar.com.br/treinar-automacao.php");
+
+            // cadastra o usuário 
+            cy.get(editUsuario).type("usuario1234");
+            cy.get(editSenha).type("1234", { log: false });
+            cy.get(editNome).type("Usuário teste 1234");
+            cy.get(btnEnviar).click();
+
+
+            cy.get(tableUsuarios).contains("Usuário teste 1234");
+            cy.get(tableUsuarios).contains("usuario1234");
+
+            cy.get(tableUsuarios).contains('tr', "usuario1234").invoke('index').then((i) => {
+                cy.get(`:nth-child(${i + 1})`).contains('Apagar').click();
+            })
         });
-        cy.visit("/");
-
-    });
-    context("Quando quando informo dados válidos", function(){
-        beforeEach(()=>{
-            cy.visit("/treinar-automacao.php");
-        });
-        
-        it("Deve retornar um cadastro válido", function(){
-            cadastroUsuarioPage.cadastrarNovoUsuario(
-                listaUsuarios.nome, 
-                listaUsuarios.usuario, 
-                listaUsuarios.senha);
-        });
-
-        afterEach(()=>{
-            cadastroUsuarioPage.validarNovoUsuario(
-                listaUsuarios.nome, 
-                listaUsuarios.usuario);
-        })
-    });
-    after(()=>{
-        cadastroUsuarioPage.excluirUsuario(listaUsuarios.usuario);
-        cy.visit("/");
     });
 });
